224b14e9264710532d5377f00363f171a8785b6c
dave75-cloud <david.j.kerr@outlook.com>
2026-02-23 23:11:20 +1000

    Update main.tf

--- infra/terraform/ecs/main.tf
+++ infra/terraform/ecs/main.tf
@@ -470,9 +470,7 @@ resource "aws_cloudwatch_metric_alarm" "api_error_logs" {
   alarm_actions       = [aws_sns_topic.alarms.arn]
 }
 
-output "alb_dns_name" {
-  value = aws_lb.alb.dns_name
-}
+
 
 output "alarms_topic_arn" {
   value = aws_sns_topic.alarms.arn
