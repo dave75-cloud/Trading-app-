f74c0a5804b87ccd31d59ddadcb8f986c282320a
dave75-cloud <david.j.kerr@outlook.com>
2026-02-23 22:30:20 +1000

    Update variables.tf

--- infra/terraform/rds/variables.tf
+++ infra/terraform/rds/variables.tf
@@ -1,11 +1,34 @@
-variable "project"           { type = string }
-variable "vpc_id"            { type = string }
-variable "private_subnets"   { type = list(string) }
-variable "ecs_security_group_id" { type = string }
-variable "db_username"       { type = string }
-variable "db_password"       { type = string }
-variable "db_instance_class" { type = string }
-variable "db_allocated_storage" { type = number }
+variable "project" { type = string }
+
+variable "vpc_id" { type = string }
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "ecs_security_group_id" {
+  type = string
+}
+
+variable "db_username" {
+  type    = string
+  default = "app"
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "db_allocated_storage" {
+  type    = number
+  default = 20
+}
 
 variable "alarms_topic_arn" {
   type    = string
