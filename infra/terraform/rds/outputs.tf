70ef733e4726d00bf59362fb09821ff08894aa21
David Kerr <davidkerr@Davids-iMac.gateway>
2026-02-24 14:53:14 +1000

    rds: add module outputs (db_url, endpoint, port, name)

--- infra/terraform/rds/outputs.tf
+++ infra/terraform/rds/outputs.tf
@@ -1,8 +1,16 @@
+output "db_endpoint" {
+  value = aws_db_instance.postgres.address
+}
+
+output "db_port" {
+  value = aws_db_instance.postgres.port
+}
+
+output "db_name" {
+  value = aws_db_instance.postgres.db_name
+}
+
 output "db_url" {
   value     = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.postgres.address}:${aws_db_instance.postgres.port}/${aws_db_instance.postgres.db_name}"
   sensitive = true
 }
-
-output "db_endpoint" { value = aws_db_instance.postgres.address }
-output "db_port"     { value = aws_db_instance.postgres.port }
-output "db_name"     { value = aws_db_instance.postgres.db_name }
