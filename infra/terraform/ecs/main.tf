37dd196e278e75cda056c453e784f34c3da1d03e
dave75-cloud <david.j.kerr@outlook.com>
2026-03-15 18:30:04 +1000

    Update main.tf

--- infra/terraform/ecs/main.tf
+++ infra/terraform/ecs/main.tf
@@ -175,6 +175,33 @@ resource "aws_iam_role_policy" "events" {
   })
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.dashboard.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "api_paths" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+
+  condition {
+    path_pattern {
+      values = local.api_paths
+    }
+  }
+}
+
 # -----------------------------
 # Logs
 # -----------------------------
