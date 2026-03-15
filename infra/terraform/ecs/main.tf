d1d0378f2c85ffe5c08c44191af1ac14253bbf39
dave75-cloud <david.j.kerr@outlook.com>
2026-03-15 18:24:59 +1000

    Update main.tf

--- infra/terraform/ecs/main.tf
+++ infra/terraform/ecs/main.tf
@@ -198,25 +198,7 @@ resource "aws_cloudwatch_log_group" "runner" {
 # Load balancer
 # -----------------------------
 
-resource "aws_acm_certificate" "this" {
-  count             = var.domain_name != "" && var.hosted_zone_id != "" ? 1 : 0
-  domain_name       = var.domain_name
-  validation_method = "DNS"
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "cert_validation" {
-  for_each = var.domain_name != "" && var.hosted_zone_id != "" ? {
-    for dvo in aws_acm_certificate.this[0].domain_validation_options :
-    dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  } : {}
 
   zone_id = var.hosted_zone_id
   name    = each.value.name
@@ -225,25 +207,6 @@ resource "aws_route53_record" "cert_validation" {
   records = [each.value.record]
 }
 
-resource "aws_route53_record" "app" {
-  count   = var.domain_name != "" && var.hosted_zone_id != "" ? 1 : 0
-  zone_id = var.hosted_zone_id
-  name    = var.domain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.alb.dns_name
-    zone_id                = aws_lb.alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "this" {
-  count = var.domain_name != "" && var.hosted_zone_id != "" ? 1 : 0
-
-  certificate_arn         = aws_acm_certificate.this[0].arn
-  validation_record_fqdns = [for r in aws_route53_record.cert_validation : r.fqdn]
-}
 
 # -----------------------------
 # Load balancer
