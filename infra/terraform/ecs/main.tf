48ae4cd1cd6282ea181d4b5134216d235e5ac14f
dave75-cloud <david.j.kerr@outlook.com>
2026-03-19 20:41:21 +1000

    Update main.tf

--- infra/terraform/ecs/main.tf
+++ infra/terraform/ecs/main.tf
@@ -284,7 +284,7 @@ resource "aws_ecs_task_definition" "api" {
         { name = "MODEL_REGISTRY", value = "/models_registry/gbpusd" },
         { name = "DATA_DIR",       value = "/data/market_candles" },
         { name = "SYMBOL",         value = "GBPUSD" },
-				{ name = "DB_URL", value = "postgresql+psycopg://postgres:SignalDb2026@gbpusd-signal-db.cpiwo4s0ilo0.ap-southeast-2.rds.amazonaws.com:5432/postgres?sslmode=require" }
+				{ name = "DB_URL", value = "postgresql+psycopg://app:SignalDb2026@gbpusd-signal-db.cpiwo4s0ilo0.ap-southeast-2.rds.amazonaws.com:5432/postgres?sslmode=require" }
       ]
       secrets = local.api_secrets
       logConfiguration = {
